69d0ca932cc33e392e80fb2a99d7aac587017345
Mustafa Shaikh <smustfa147@gmail.com>
2021-12-09 15:32:49 +0530

    Main: Add post service to project
    This service can be used for all API calls get,post,put,delete just add a parameter in request to state the method

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  url='http://localhost:80/BE/';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  post(path,data) {
+    return this.httpClient.post(this.url + path, JSON.stringify(data), this.httpOptions)
+    .map(res => (res as any));
+  }
+}
