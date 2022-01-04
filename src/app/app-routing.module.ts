7b3cd0529a8601fdfc367595cb571d0c824cc533
Panda <preranadhotre2000@gmail.com>
2022-01-04 23:51:30 +0530

    login: login user/admin through same page
    Check login credentials and return result, if user is admin navigate to admin-panel else user-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,19 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'forgot',
+    loadChildren: () => import('./forgot/forgot.module').then( m => m.ForgotPageModule)
+  },
+
 ];
 
 @NgModule({
